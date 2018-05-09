e6baecc0d960463d45f88bca0f9104df5019bd0f
kandalab <39138479+kandalab@users.noreply.github.com>
2018-05-09 17:46:25 -0400

    Update PKPDM-IV-1CMT-EFF.cpp

--- PKPDM-IV-1CMT-EFF.cpp
+++ PKPDM-IV-1CMT-EFF.cpp
@@ -3,44 +3,34 @@ $PROB IV ONE COMP PKPD EFFECT COMPARTMENT MODEL
 $PARAM @annotated
 CL 		: 5   : Clearance (L/hr)
 V1 		: 25  : Central Volume (L)
-E0 	    : 157 : Input Rate Constant (mg/hr)
-EC50 	: 5   : Concentration at Half-maximal Effect (mg/L)
-EMAX 	: 30  : Maximum Effect (units)
-KEO 	: 1.3 : Effect compartment rate constant (1/hr)
+
 
 $OMEGA @annotated @block
 ETA_CL      : 0.01 : ETA on CL
 ETA_V1      : 0 0 0.01 : ETA on V1
-ETA_E0      : 0 0 0 0 0.01 : ETA on E0
-ETA_EC50    : 0 0 0 0 0 0.01 : ETA on EC50
-ETA_EMAX    : 0 0 0 0 0 0 0.01 : ETA on EMAX
-ETA_KEO		: 0 0 0 0 0 0 0 0.01 : ETA on KEO  
+
 
 
 $CMT @annotated
 CENT    : Central (mass)
-EFF     : PD Response 
+
 
 $MAIN
 double CLi = CL * exp(ETA_CL);
 double V1i = V1 * exp(ETA_V1);
-double E0i = E0 * exp(ETA_E0);
-double EC50i = EC50 * exp(ETA_EC50);
-double EMAXi = EMAX * exp(ETA_EMAX);
-double KEOi = KEO * exp(ETA_KEO);
 
 
 
 
 $ODE
 dxdt_CENT = - (CLi/V1i)*CENT;
-dxdt_EFF = KEOi * (CENT - EFF);
+
 
 $TABLE
 double CENTRAL = CENT/V1i 
-double EFFECT = (E0i - (EMAXi*EFF)/(EFF+EC50i)) 
+
 
 $CAPTURE @annotated
 CENTRAL    : Plasma Concentration (mass/time)
-EFFECT     : PD response (units)
+
   
